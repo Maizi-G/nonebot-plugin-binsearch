716c220caf2da8ffeef5f87d92189a191fad55fa
Mai Zi <157226607+Maizi-G@users.noreply.github.com>
2025-10-12 13:51:22 +0800

    Fate:允许设置黑名单用户，支持多api负载均衡

--- nonebot_plugin_binsearch/config.py
+++ nonebot_plugin_binsearch/config.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 
 
 class Config(BaseModel):
-    bin_api_key: str
+    bin_api_key: list[str]
     # 忽略的 QQ 用户 ID 列表（字符串或数字均可）
     ignore_user_ids: list[str] = [3938088854]
